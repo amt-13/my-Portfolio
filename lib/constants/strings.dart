f84f843047c86ca708561bfe21a95e3dd3d0582a
amt <amt.arasan@gmail.com>
2022-01-24 11:44:11 +0530

    Commit for Portfolio Files added

---
+++ lib/constants/strings.dart
@@ -0,0 +1,38 @@
+class Strings {
+  Strings._();
+
+  static const String about_me = 'About Me';
+  static const String about = 'About';
+  static const String me = ' Me';
+  static const String portfoli = 'Portfoli';
+  static const String o = 'o';
+  static const String headline = 'I\'am Tamilarasn Murugesan, Windows Application/Pluging Developer from Chennai, India';
+  static const String summary = 'Focused professional having excellent technical and communication skills, and having 7 years of experience in Automotive Industry.';
+  static const String experience = 'I have been Working in Application for Analysis of photometric simulation of beam Pattern with specific configuration settings like dimming , aiming.
+And Spec and Regulations Report Generation with Different Standards like ECE , US ,IIHS.., @ Valeo India
+I Have worked Catia Logical and Physical Tree structure Link and Configuration and Effectivity Management using CAA 
+worked in Creating the Logical structure in Catia Batch Applications.
+
+Developed CATIA V5 Batch application FAST Export / Import for Create the Product structure in V5 Using XPDMXML file and Link the CATParts with the Structures and and translate the all Parts with global axis.
+
+Developed and implement the IDL Interface to Expose the Customized CAA API to CATScripts User.
+Developed and implement the Interface to Expose the Customized CAA API to CATRule(EKL).
+
+I have developed CAA Command for V6 to User Restriction of the CATMacros and CATScripts.
+with the command only authorized users can access the CATMacros and CATScripts. 
+I have been developing 3dExcite (Deltagen) Customize Plugins and Standalone Application.
+I am developer for Standalone Applications development and 3DExcite(Deltagen) Plugins development and integration.
+One of the Key Development False Color Rendering which is Standalone Application 
+Helps to measure the luminance of a reflection and Analyze the impact of glare an reflections in upstream phase
+Interior lighting behavior analysis and Homogeneity of Cockpit lighting
+Light leakage analysis and Clear understanding of luminance levels inside the cockpit. @ Renault';
+
+  static const String skills_i_have = 'C++ , Qt5 , Qt Creator , Qt Designer , OpenCV , OpenEXR , FFMpeg';
+  static const String rights_reserved = '© 2019 ALL RIGHTS RESERVED.';
+
+  // menu items
+  static const String menu_home = 'Home';
+  static const String menu_about = 'About';
+  static const String menu_contact = 'Contact';
+
+}
